6aa8e6f7689400d8e5686203cb51de37bd675caf
raslanrizvi <raslanrizvi05@gmail.com>
2023-03-10 03:25:49 +0530

    created edit product

--- editproduct2.php
+++ editproduct2.php
@@ -1,3 +1,11 @@
+
+<?php
+
+// connecting the Database
+    require("db_connection.php");
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,7 +26,7 @@
     <link rel="stylesheet" type="text/css" href="css/nz_admin_dashboard.css"/>
     <!-- End Styles -->
 
-    <title>Add New Product</title>
+    <title>Edit Product</title>
 
 
 </head>
@@ -28,7 +36,7 @@
         <div class="navigation">
             <ul>
                 <li>
-                    <a href="">
+                    <a>
                         <span class="icon"></span>
                         <span class="title">NZ F&C Dashboard</span>
                     </a>
@@ -40,19 +48,19 @@
                     </a>
                 </li>
                 <li>
-                    <a href="">
+                    <a>
                         <span class="icon"><ion-icon name="people-outline"></ion-icon></span>
                         <span class="title">Employee Management</span>
                     </a>
                 </li>
                 <li class="activeNav">
-                    <a href="">
+                    <a>
                         <span class="icon"><ion-icon name="basket-outline"></ion-icon></span>
                         <span class="title">Product Management</span>
                     </a>
                 </li>
                 <li>
-                    <a href="">
+                    <a>
                         <span class="icon"><ion-icon name="receipt-outline"></ion-icon></span>
                         <span class="title">Order Management</span>
                     </a>
@@ -83,21 +91,49 @@
             <div class="content">
                 <div class="mainCardAddPdt">
                     <div class="addPdtForm">
-                        <form action="addproduct2.php" method="post" enctype="multipart/form-data">
+
+                    <?php
+                        // echo "<pre>";
+                        // print_r($_REQUEST);
+                        // echo "</pre>";
+
+
+                    // Searching for the record
+                        $ptd_id = $_REQUEST['ptd_id'];
+
+                    // building dynamic SQL Code
+                        $sql = "select * from product where ptd_id=$ptd_id";
+
+                    // execute the SQL code
+                        $rs = $mysqli->query($sql);
+
+                        $record_count = mysqli_num_rows($rs);
+
+                        if(mysqli_num_rows($rs) > 0){
+
+                            $row = mysqli_fetch_assoc($rs);
+
+                                // echo "<pre>";
+                                // print_r($row);
+                                // echo "</pre>";
+                    ?>
+
+                        <form action="editproduct3.php" method="post">
                             <div class="custom-file imgUpload-grp">
                                 <label class="custom-file-label" for="name">Upload Product Image</label>
                                 <input type="file" class="custom-file-input imgUpload" name="picture1" id="picture1">
                             </div>
                             <div class="form-group row">
+                                <input type="hidden" name="ptd_id" value="<?php echo $row['ptd_id']; ?>">
                                 <label for="name" class="col-sm-2 col-form-label">Title of the Product</label>
                                 <div class="col-sm-10">
-                                  <input type="text" class="form-control addPdtTB" name="title" id="title" placeholder="Make the Title Strong">
+                                  <input type="text" class="form-control addPdtTB" name="title" id="title" placeholder="Make the Title Strong" value="<?php echo $row['title']; ?>">
                                 </div>
                             </div>
                             <div class="form-group row">
                                 <label for="name" class="col-sm-2 col-form-label">Brand of the product</label>
                                 <div class="col-sm-10">
-                                  <input type="text" class="form-control addPdtTB" name="brand" id="pdtBrand" placeholder="Brand of the Product">
+                                  <input type="text" class="form-control addPdtTB" name="brand" id="pdtBrand" placeholder="Brand of the Product" value="<?php echo $row['brand']; ?>">
                                 </div>
                             </div>
                             <div class="form-group row">
@@ -109,42 +145,42 @@
                                 <div class="col-sm-10 pdtSizeDropdown">
                                     <div class="form-check checkbox-inline">
                                         <input type="hidden" name="size_xs" value="off" >
-                                        <input class="form-check-input addPdtTB" type="checkbox" name="size_xs" id="pdtSizeXS">
+                                        <input class="form-check-input addPdtTB" type="checkbox" name="size_xs" id="pdtSizeXS" <?php if ($row['size_xs'] == "on") echo "checked"; ?>>
                                         <label class="form-check-label" for="name">
                                         XS
                                         </label>
                                     </div>
                                     <div class="form-check checkbox-inline">
                                         <input type="hidden" name="size_s" value="off" >
-                                        <input class="form-check-input addPdtTB" type="checkbox" name="size_s" id="pdtSizeS">
+                                        <input class="form-check-input addPdtTB" type="checkbox" name="size_s" id="pdtSizeS" <?php if ($row['size_s'] == "on") echo "checked"; ?>>
                                         <label class="form-check-label" for="name">
                                         S
                                         </label>
                                     </div>
                                     <div class="form-check checkbox-inline">
                                         <input type="hidden" name="size_m" value="off" >
-                                        <input class="form-check-input addPdtTB" type="checkbox" name="size_m" id="pdtSizeM">
+                                        <input class="form-check-input addPdtTB" type="checkbox" name="size_m" id="pdtSizeM" <?php if ($row['size_m'] == "on") echo "checked"; ?>>
                                         <label class="form-check-label" for="name">
                                         M
                                         </label>
                                     </div>
                                     <div class="form-check checkbox-inline">
                                         <input type="hidden" name="size_l" value="off" >
-                                        <input class="form-check-input addPdtTB" type="checkbox" name="size_l" id="pdtSizeL">
+                                        <input class="form-check-input addPdtTB" type="checkbox" name="size_l" id="pdtSizeL" <?php if ($row['size_l'] == "on") echo "checked"; ?>>
                                         <label class="form-check-label" for="name">
                                         L
                                         </label>
                                     </div>
                                     <div class="form-check checkbox-inline">
                                         <input type="hidden" name="size_xl" value="off" >
-                                        <input class="form-check-input addPdtTB" type="checkbox" name="size_xl" id="pdtSizeXL">
+                                        <input class="form-check-input addPdtTB" type="checkbox" name="size_xl" id="pdtSizeXL" <?php if ($row['size_xl'] == "on") echo "checked"; ?>>
                                         <label class="form-check-label" for="name">
                                         XL
                                         </label>
                                     </div>
                                     <div class="form-check checkbox-inline">
                                         <input type="hidden" name="size_xxl" value="off" >
-                                        <input class="form-check-input addPdtTB" type="checkbox" name="size_xxl" id="pdtSizeXXL">
+                                        <input class="form-check-input addPdtTB" type="checkbox" name="size_xxl" id="pdtSizeXXL" <?php if ($row['size_xxl'] == "on") echo "checked"; ?>>
                                         <label class="form-check-label" for="name">
                                         XXL
                                         </label>
@@ -155,7 +191,7 @@
                                 <div class="col-auto my-1">
                                   <label class=" mr-sm-2" for="name">Product Category</label>
                                     <select class="custom-select addPdtCtgryDrpDwn mr-sm-10" name="ctgy" id="ctgy">
-                                        <option selected>Select a Category</option>
+                                        <option selected><?php echo $row['ctgy']; ?></option>
                                         <option value="KIMONO JACKET">KIMONO JACKET</option>
                                         <option value="LONGWEAR JACKET">LONGWEAR JACKET</option>
                                         <option value="STREETWEAR PANTS">STREETWEAR PANTS</option>
@@ -173,14 +209,14 @@
                                 <div class="col-sm-10 pdtSizeDropdown">
                                     <div class="form-check checkbox-inline">
                                         <input type="hidden" name="new_arrivals" value="off" >
-                                        <input class="form-check-input addPdtTB" type="checkbox" name="new_arrivals" id="pdtnewarrivals">
+                                        <input class="form-check-input addPdtTB" type="checkbox" name="new_arrivals" id="pdtnewarrivals" <?php if ($row['new_arrivals'] == "on") echo "checked"; ?>>
                                         <label class="form-check-label" for="name">
                                         New Arrivals
                                         </label>
                                     </div>
                                     <div class="form-check checkbox-inline">
                                         <input type="hidden" name="sale" value="off" >
-                                        <input class="form-check-input addPdtTB" type="checkbox" name="sale" id="pdtSale">
+                                        <input class="form-check-input addPdtTB" type="checkbox" name="sale" id="pdtSale" <?php if ($row['sale'] == "on") echo "checked"; ?>>
                                         <label class="form-check-label" for="name">
                                         Sale
                                         </label>
@@ -191,36 +227,57 @@
                                 <label for="name" class="col-sm-2 col-form-label">Quantity</label>
                                 <div class="col-sm-10">
                                     <div class="value-button value-buttonMinus" id="decrease" onclick="decreaseValue()" value="Decrease Value">-</div>
-                                    <input type="number" name="qty" id="number" value="0" />
+                                    <input type="number" name="qty" id="number" value="<?php echo $row['qty']; ?>" />
                                     <div class="value-button value-buttonAdd" id="increase" onclick="increaseValue()" value="Increase Value">+</div>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="name">Description</label>
-                                <textarea class=" addPdt-Dscrpt form-control addPdtTB addPdtTB-Dscrpt" name="description" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                <textarea class=" addPdt-Dscrpt form-control addPdtTB addPdtTB-Dscrpt" name="description" id="exampleFormControlTextarea1" rows="3"><?php echo $row['description']; ?></textarea>
                               </div>
                             <div class="form-group row">
                                 <label for="name" class="col-sm-2 col-form-label">Price</label>
                                 <div class="col-sm-10">
-                                  <input type="number" class="form-control addPdtTB" name="price" id="pdtTitle" placeholder="1999.99">
+                                  <input type="number" class="form-control addPdtTB" name="price" id="pdtTitle" placeholder="1999.99" value="<?php echo $row['price']; ?>">
                                 </div>
                             </div>
                             <div class="form-group row">
                                 <label for="name" class="col-sm-2 col-form-label">Sale Price</label>
                                 <div class="col-sm-10">
                                     <input type="hidden" name="sale_price" value="0" >
-                                  <input type="number" class="form-control addPdtTB" name="sale_price" id="pdtSaletb" disabled placeholder="1999.99">
+                                  <input type="number" class="form-control addPdtTB" name="sale_price" id="pdtSaletb" <?php if ($row['sale'] == "off") echo "disabled"; ?> placeholder="1999.99" value="<?php echo $row['sale_price']; ?>">
                                 </div>
                             </div>
                             <div class="frmbtn">
-                                    <input type="submit" class="addPdtbtn" name="submit" id="submit" value="ADD PRODUCT">
+                                    <input type="submit" class="addPdtbtn" name="submit" id="submit" value="Update Product">
                                     <input type="reset" class="resetPdtbtn" name="reset"  id="reset" value="RESET">
-                                    <a class="homePdtbtn" href="nz_admin_dashboard.php">
-                                        Back to Home
+                                    <a class="homePdtbtn" href="editproduct.php">
+                                        Back
                                     </a>
                                 </div>
                             </div>
                         </form>
+
+
+                    <?php
+                        }
+                        else{
+                    ?>
+
+                            <div class="alert alert-danger rounded-pill" role="alert">
+                                <h4 class="alert-heading">No Matching Records Found</h4>
+                                <p>Please check if the Product ID is correct and Try Again.</p>
+                                <hr>
+                                <a href="editproduct.php" class="resetPdtbtn">Try Again</a>
+                                <a href="nz_admin_dashboard.php" class="homePdtbtn">Back To Home</a>
+                            </div>
+
+                    <?php
+                        }
+
+                    ?>
+
+
                     </div>
                 </div>
             </div>
---
+++ editproduct3.php
@@ -0,0 +1,61 @@
+<?php
+
+    // connecting to the Database
+        require("db_connection.php");
+
+    // Getting the values from the edit product 2 page
+        // echo "<pre>";
+        //     print_r($_REQUEST);
+        // echo "</pre>";
+
+    // copy from the field values to the variables
+        $ptd_id       = $_REQUEST['ptd_id'];
+        $title        = $_REQUEST['title'];
+        $brand        = $_REQUEST['brand'];
+        $size_xs      = $_REQUEST['size_xs'];
+        $size_s       = $_REQUEST['size_s'];
+        $size_m       = $_REQUEST['size_m'];
+        $size_l       = $_REQUEST['size_l'];
+        $size_xl      = $_REQUEST['size_xl'];
+        $size_xxl     = $_REQUEST['size_xxl'];
+        $ctgy         = $_REQUEST['ctgy'];
+        $new_arrivals = $_REQUEST['new_arrivals'];
+        $sale         = $_REQUEST['sale'];
+        $qty          = $_REQUEST['qty'];
+        $description  = $_REQUEST['description'];
+        $price        = $_REQUEST['price'];
+        $sale_price   = $_REQUEST['sale_price'];
+
+    // Building the Dynamic SQL Commands
+        $sql  = "update product set ";
+        $sql .= "title='$title',";
+        $sql .= "brand='$brand',";
+        $sql .= "size_xs='$size_xs',";
+        $sql .= "size_s='$size_s',";
+        $sql .= "size_m='$size_m',";
+        $sql .= "size_l='$size_l',";
+        $sql .= "size_xl='$size_xl',";
+        $sql .= "size_xxl='$size_xxl',";
+        $sql .= "ctgy='$ctgy',";
+        $sql .= "new_arrivals='$new_arrivals',";
+        $sql .= "sale='$sale',";
+        $sql .= "qty=$qty,";
+        $sql .= "description='$description',";
+        $sql .= "price=$price,";
+        $sql .= "sale_price=$sale_price where ptd_id=$ptd_id";
+
+
+    // Execute the SQL Command
+        $x = $mysqli->query($sql);
+
+        if($x > 0){
+            // echo "successfully Updated";
+            header("location:editproduct4.php?status=pass");
+        }
+        else{
+            // echo "Saving Changes Failed";
+            header("location:editproduct4.php?status=fail");
+        }
+
+
+?>
